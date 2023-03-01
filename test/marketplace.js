46e9f989f6a1c2631d6bcf20befd2e4389942ce7
Helme0315 <nakamurashota0315@gmail.com>
2023-03-02 04:39:38 +0900

    update

--- test/marketplace.js
+++ test/marketplace.js
@@ -2,7 +2,6 @@ const { expect } = require("chai");
 const { ethers } = require("hardhat");
 
 describe("Marketplace", function () {
-  let blt;
   let nft;
   let market;
   let admin;
@@ -73,97 +72,4 @@ describe("Marketplace", function () {
       .withArgs(nft.address, 0, 0, player1.address);
   });
 
-  // it("Buy an NFT", async function () {
-  //   // Add a new isting
-  //   await nft.connect(player1).approve(market.address, 0);
-  //   await market.connect(admin).setWhitelistTokens(["0x81f91E226c663574b4619926Bea0DDe889d0fe08"]);
-  //   await market
-  //     .connect(player1)
-  //     .addListing(0, ethers.utils.parseUnits("5"), "0x81f91E226c663574b4619926Bea0DDe889d0fe08");
-  //   await nft.connect(player1).approve(market.address, 1);
-  //   await market
-  //     .connect(player1)
-  //     .addListing(1, ethers.utils.parseUnits("10"), "0x81f91E226c663574b4619926Bea0DDe889d0fe08");
-
-  //   // Get the total count of listings.
-  //   let totalListings = await market.activeListingCount();
-  //   expect(totalListings.toNumber()).to.equal(2);
-  //   const listingId = 2;
-  //   const listing = await market.listings(listingId);
-
-  //   // Approve BLT and But NFT from the marketplace.
-  //   await blt.connect(player2).approve(market.address, listing.price);
-  //   await expect(market.connect(player2).buy(listingId))
-  //     .to.emit(market, "ItemSold")
-  //     .withArgs(2, 1, player1.address, player2.address, listing.price, 0, 0);
-  //   totalListings = await market.activeListingCount();
-  //   expect(totalListings.toNumber()).to.equal(1);
-
-  //   // Check NFT was exchanged.
-  //   expect(await nft.ownerOf(1)).to.equal(player2.address); 
-
-  //   // Check BLT was paid from player2 to player1
-  //   expect(await blt.balanceOf(player1.address)).to.equal(
-  //     ethers.utils.parseUnits("10")
-  //   );
-  //   expect(await blt.balanceOf(player2.address)).to.equal(
-  //     ethers.utils.parseUnits("90")
-  //   );
-  // });
-
-  // it("Add Fees", async function () {
-  //   await expect(
-  //     market
-  //       .connect(admin)
-  //       .setFee(200, treasury1.address, 50, treasury2.address)
-  //   )
-  //     .to.emit(market, "FeesChanged")
-  //     .withArgs(200, treasury1.address, 50, treasury2.address, admin.address);
-  //   const listingId = 1;
-  //   const listing = await market.listings(listingId);
-  //   await blt.connect(player2).approve(market.address, listing.price);
-  //   await expect(market.connect(player2).buy(listingId))
-  //     .to.emit(market, "ItemSold")
-  //     .withArgs(
-  //       1,
-  //       0,
-  //       player1.address,
-  //       player2.address,
-  //       listing.price,
-  //       ethers.utils.parseUnits("0.1"),
-  //       ethers.utils.parseUnits("0.025")
-  //     );
-  //   expect((await market.activeListingCount()).toNumber()).to.equal(0);
-
-  //   // Check BLT was paid from player2 to player1 and Fees were applied
-  //   expect(await blt.balanceOf(player1.address)).to.equal(
-  //     ethers.utils.parseUnits("14.875")
-  //   );
-  //   expect(await blt.balanceOf(player2.address)).to.equal(
-  //     ethers.utils.parseUnits("85")
-  //   );
-  //   expect(await blt.balanceOf(treasury1.address)).to.equal(
-  //     ethers.utils.parseUnits("99999900.1")
-  //   );
-  //   expect(await blt.balanceOf(treasury2.address)).to.equal(
-  //     ethers.utils.parseUnits("0.025")
-  //   );
-  // });
-
-  // it("Pause contract", async () => {
-  //   await market.connect(admin).pause(true);
-  //   await expect(
-  //     market
-  //       .connect(player1)
-  //       .addListing(0, ethers.utils.parseUnits("2"), blt.address)
-  //   ).to.be.revertedWith("Pausable: paused");
-  //   await expect(market.connect(player1).buy(0)).to.be.revertedWith(
-  //     "Pausable: paused"
-  //   );
-  //   await market.connect(admin).pause(false);
-  //   await nft.connect(player1).approve(market.address, 2);
-  //   await market
-  //     .connect(player1)
-  //     .addListing(2, ethers.utils.parseUnits("10"), blt.address);
-  // });
 });
